99c20f3d8eabbb8d65b96202c92ac33af2d0d178
roadiz-ci <ambroise+ci@roadiz.io>
2024-12-06 08:58:03 +0000

    Merge branch release/v2.4.0

--- src/Explorer/TagExplorerItem.php
+++ src/Explorer/TagExplorerItem.php
@@ -2,20 +2,43 @@
 
 declare(strict_types=1);
 
-namespace Themes\Rozier\Models;
+namespace Themes\Rozier\Explorer;
 
 use RZ\Roadiz\CoreBundle\Entity\Tag;
+use RZ\Roadiz\CoreBundle\Explorer\AbstractExplorerItem;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-final class TagModel implements ModelInterface
+final class TagExplorerItem extends AbstractExplorerItem
 {
     public function __construct(
         private readonly Tag $tag,
-        private readonly UrlGeneratorInterface $urlGenerator
+        private readonly UrlGeneratorInterface $urlGenerator,
     ) {
     }
 
-    public function toArray(): array
+    protected function getEditItemPath(): ?string
+    {
+        return $this->urlGenerator->generate('tagsEditPage', [
+            'tagId' => $this->tag->getId(),
+        ]);
+    }
+
+    protected function getColor(): ?string
+    {
+        return $this->tag->getColor();
+    }
+
+    public function getId(): string|int
+    {
+        return $this->tag->getId();
+    }
+
+    public function getAlternativeDisplayable(): ?string
+    {
+        return $this->getTagParents($this->tag);
+    }
+
+    public function getDisplayable(): string
     {
         $firstTrans = $this->tag->getTranslatedTags()->first();
         $name = $this->tag->getTagName();
@@ -24,25 +47,14 @@ final class TagModel implements ModelInterface
             $name = $firstTrans->getName();
         }
 
-        $result = [
-            'id' => $this->tag->getId(),
-            'name' => $name,
-            'tagName' => $this->tag->getTagName(),
-            'color' => $this->tag->getColor(),
-            'parent' => $this->getTagParents($this->tag),
-            'editUrl' => $this->urlGenerator->generate('tagsEditPage', [
-                'tagId' => $this->tag->getId()
-            ]),
-        ];
-
-        return $result;
+        return $name;
     }
 
-    /**
-     * @param Tag $tag
-     * @param bool $slash
-     * @return string
-     */
+    public function getOriginal(): Tag
+    {
+        return $this->tag;
+    }
+
     private function getTagParents(Tag $tag, bool $slash = false): string
     {
         $result = '';
@@ -57,7 +69,7 @@ final class TagModel implements ModelInterface
                 $name = $firstTrans->getName();
             }
 
-            $result = $superParent . $name;
+            $result = $superParent.$name;
 
             if ($slash) {
                 $result .= ' / ';
