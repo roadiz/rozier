c2281eea329b2a6e7c43a0bc688e66b69fae2ed7
roadiz-ci <ambroise+ci@roadiz.io>
2026-08-16 14:14:20 +0000

    Merge branch hotfix/2.7.37

---
+++ app/containers/TagsEditorContainer.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <slot></slot>
+        <label class="typo__label">Tagging</label>
+        <multiselect
+            v-model="value"
+            tag-placeholder="Add this as new tag"
+            placeholder="Search or add a tag"
+            label="name"
+            track-by="id"
+            :options="options"
+            :multiple="true"
+            :taggable="true"
+            group-values="children"
+            group-label="name"
+            @tag="addTag"
+            @input="tagsUpdateValue"
+        >
+        </multiselect>
+        <pre class="language-json"><code>{{ value  }}</code></pre>
+    </div>
+</template>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<script>
+import { mapState, mapActions } from 'vuex'
+
+// Components
+import Multiselect from 'vue-multiselect'
+
+export default {
+    mounted() {
+        this.tagsInitData()
+    },
+    computed: {
+        ...mapState({
+            value: (state) => state.tags.value,
+            options: (state) => state.tags.options,
+        }),
+    },
+    methods: {
+        ...mapActions(['tagsUpdateValue', 'tagsInitData']),
+        addTag(newTag) {
+            const tag = {
+                name: newTag,
+            }
+
+            this.options.push(tag)
+            this.value.push(tag)
+
+            this.tagsUpdateValue(this.value)
+        },
+    },
+    components: {
+        Multiselect,
+    },
+}
+</script>
---
+++ app/store/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import {
+    KEYBOARD_EVENT_ESCAPE,
+    LOGIN_CHECK_DISCONNECTED,
+    HEALTH_CHECK_FAILED,
+    LOGIN_CHECK_CONNECTED,
+    HEALTH_CHECK_SUCCEEDED,
+} from '../types/mutationTypes'
+
+// Modules
+import nodesSourceSearch from './modules/NodesSourceSearchStoreModule'
+import explorer from './modules/ExplorerStoreModule'
+import drawers from './modules/DrawersStoreModule'
+import filterExplorer from './modules/FilterExplorerStoreModule'
+import tags from './modules/TagsStoreModule'
+import documentPreview from './modules/DocumentPreviewStoreModule'
+import blanchetteEditor from './modules/BlanchetteEditorStoreModule'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+    modules: {
+        nodesSourceSearch,
+        explorer,
+        filterExplorer,
+        drawers,
+        tags,
+        documentPreview,
+        blanchetteEditor,
+    },
+    state: {
+        translations: window.RozierConfig.messages,
+        connected: true,
+        healthChecked: true,
+    },
+    mutations: {
+        [LOGIN_CHECK_CONNECTED](state) {
+            state.connected = true
+        },
+        [LOGIN_CHECK_DISCONNECTED](state) {
+            state.connected = false
+        },
+        [HEALTH_CHECK_SUCCEEDED](state) {
+            state.healthChecked = true
+        },
+        [HEALTH_CHECK_FAILED](state) {
+            state.healthChecked = false
+        },
+    },
+    actions: {
+        escape({ commit }) {
+            commit(KEYBOARD_EVENT_ESCAPE)
+        },
+    },
+})
